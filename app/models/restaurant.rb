7d41e6b21950eaee0ab05155c625f74d45f5a011
Voyotel <sidharth@voyotel.com>
2020-02-23 23:31:02 +0000

    Updated fog

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,5 @@
 class Restaurant < ApplicationRecord
      mount_uploader :photo, PhotoUploader
      belongs_to :account
-     # has_many :restaurant_photos
-     # has_many :restaurants, through: :restaurant_photos
      scope :latest, -> { order created_at: :desc}
 end
