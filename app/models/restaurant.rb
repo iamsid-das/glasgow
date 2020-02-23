f3d5c6a7153997a1d28524bee25cbcf745e5248a
Voyotel <sidharth@voyotel.com>
2020-02-23 13:07:50 +0000

    Added Image Uploader

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,7 @@
 class Restaurant < ApplicationRecord
      mount_uploader :photo, PhotoUploader
      belongs_to :account
-
+     has_many :restaurant_photos
+     has_many :restaurants, through: :restaurant_photos
      scope :latest, -> { order created_at: :desc}
 end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,10 @@
 CarrierWave.configure do |config|
     config.fog_credentials = {
       provider:              'AWS',                                         # required
-      aws_access_key_id:     'AKIAIMUINA7W4V2DJJFA',                        # required unless using use_iam_profile
-      aws_secret_access_key: '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',    # required unless using use_iam_profile
+      aws_access_key_id:     'AKIAJN6QW7RY72TOMUCA',                        # required unless using use_iam_profile
+      aws_secret_access_key: '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',    # required unless using use_iam_profile
       use_iam_profile:       true,                                          # optional, defaults to false
-      region:                'EU (London)',                                 # optional, defaults to 'us-east-1'
+      region:                'us-east-1',                                 # optional, defaults to 'us-east-1'
       host:                  's3.example.com',                              # optional, defaults to nil
       endpoint:              'https://s3.example.com:8080'                  # optional, defaults to nil
     }
