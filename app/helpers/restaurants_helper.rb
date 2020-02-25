78bc5f891d4b1df2a33bf54b99ca50989135c2ba
Voyotel <sidharth@voyotel.com>
2020-02-25 05:06:02 +0000

    Modified Restaurant view

--- app/helpers/restaurants_helper.rb
+++ app/helpers/restaurants_helper.rb
@@ -1,6 +1,5 @@
 module RestaurantsHelper
     def restaurant_thumbnail
-        
         img = restaurant.photo.present? ? restaurant.photo.thumb.url : "placeholder.png"
         image_tag img, class: "restaurant-thumb"
     end
