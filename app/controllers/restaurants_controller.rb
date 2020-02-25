481e5c143dc4f9ebd414d1e571e9efb1bc77f75f
Voyotel <sidharth@voyotel.com>
2020-02-25 23:19:33 +0000

    Updated restaurant controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -8,10 +8,6 @@ class RestaurantsController < ApplicationController
     @restaurants = Restaurant.all
   end
   
-  def restaurant_thumbnail
-    img = thumbnail.present? ? restaurant.photo.thumbnail.url : "placeholder.png"
-    image_tag img, class: "restaurant-thumbnail"
-  end
 
   # GET /restaurants/1
   # GET /restaurants/1.json
